0e10ba213b7b27e2ae83723a6013c80e3cb33cc3
hector <hmg701@yahoo.com>
2020-09-06 13:18:21 -0500

    Added loading icon

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { ScrollView, StyleSheet, Linking } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Linking,
+  ActivityIndicator,
+} from "react-native";
 
 import { url } from "./url";
 import axios from "axios";
@@ -32,22 +37,46 @@ class App extends React.Component {
     });
     return (
       <ScrollView style={styles.scrollView}>
-        {finalData.length > 0
-          ? finalData.map((card, index) => {
-              return (
-                <Card
-                  key={index}
-                  image={card.urlToImage}
-                  title={card.title}
-                  onPress={this.openLink.bind(this, card.url)}
-                />
-              );
-            })
-          : null}
+        {finalData.length > 0 ? (
+          finalData.map((card, index) => {
+            return (
+              <Card
+                key={index}
+                image={card.urlToImage}
+                title={card.title}
+                onPress={this.openLink.bind(this, card.url)}
+              />
+            );
+          })
+        ) : (
+          <Container>
+            <ActivityIndicator size="large" color="#03396c" />
+            <Title>Loading News...</Title>
+          </Container>
+        )}
       </ScrollView>
     );
   }
 }
+
+const Container = styled.View`
+  border-radius: 14px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+  flex: 1;
+  display: flex;
+  margin-top: 400px;
+`;
+
+const Title = styled.Text`
+  color: black;
+  font-size: 28px;
+  font-weight: bold;
+  margin-top: 10px;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.8);
+`;
 
 const styles = StyleSheet.create({
   scrollView: {
